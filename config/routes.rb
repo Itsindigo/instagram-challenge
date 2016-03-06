d017710d185f323edbc203214272961a6f5f2009
itsindigo <matthewbridgesbusiness@gmail.com>
2016-03-06 15:37:17 +0000

    Added oauth plus devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,10 +11,13 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  resources :photos, shallow: true
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  resources :photos
 
 
   root to: "photos#index"
+
 
   # Example resource route with options:
   #   resources :products do
