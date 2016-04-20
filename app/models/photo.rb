90c9452fcfa0e335ea78708e067f5042b3f11630
Matthew Bridges <matthewbridges@Matthews-MacBook-Pro-2.local>
2016-04-20 13:54:41 +0100

    Got s3 working, changed region

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :square => "200x200>" }, :default_url => "/images/:style/missing.png",
+                    :url =>':s3_domain_url', :path => '/:class/:attachment/:id_partition/:style/:filename',
                     :storage => :s3, :bucket => "rails-impostergram", :s3_credentials => {:access_key_id => ENV["AWS_KEY"], :secret_access_key => ENV["AWS_SECRET"]}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
